cfb2fa882a70e773c6f2e0b9998486b0333ce37d
KHL562 <khl562sk@gmail.com>
2019-02-13 21:23:26 -0800

    final

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Charlie",
+		photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.sunnyskyz.com%2Fuploads%2F2015%2F02%2F3wznj-vet3.jpg&imgrefurl=https%3A%2F%2Fwww.sunnyskyz.com%2Fblog%2F699%2F14-Priceless-Photos-Of-Dogs-Realizing-They-re-Going-To-The-Vet-Not-The-Park&docid=PQyzSJYdVMBNmM&tbnid=g2O0UPEQPTH6EM%3A&vet=10ahUKEwjvuu_UvLrgAhWD_p8KHZ4LDjgQMwgxKAgwCA..i&w=600&h=393&safe=active&bih=937&biw=1920&q=dog%20faces&ved=0ahUKEwjvuu_UvLrgAhWD_p8KHZ4LDjgQMwgxKAgwCA&iact=mrc&uact=8",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Jack",
+		photo: "https://www.dailymail.co.uk/femail/article-4696984/Dog-owners-pull-dogs-faces-HILARIOUS-pictures.html",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Jerry",
+		photo: "https://img.yespets.com/filter:scale/quill/e/f/d/0/d/4/efd0d4a1/2286e1f38ead1862810dec8685286d6aa89f34d2.jpg?mw=650",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Mac",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_ScJsl9gpQ5t3csuEall-XZRUoNumBDhRChx_YKrNkxK1hlAt",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Freddy",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ69BXN-Bzkkoz6n6zf7RuuEgPriilBXgdDyL8INzU6Qxb60oRW",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
